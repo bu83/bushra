5edfb19d7d5f173c67a08bbbc656d80a7f7700f3
bu83 <84966528+bu83@users.noreply.github.com>
2021-05-28 23:45:44 -0700

    v2

--- lab.py
+++ lab.py
@@ -4,22 +4,15 @@ weather=['Sunny','Sunny','Overcast','Rainy','Rainy','Rainy','Overcast','Sunny','
 'Rainy','Sunny','Overcast','Overcast','Rainy']
 # Second Feature
 temp=['Hot','Hot','Hot','Mild','Cool','Cool','Cool','Mild','Cool','Mild','Mild','Mild','Hot','Mild']
-
-
-/////////
 play=['No','No','Yes','Yes','Yes','No','Yes','No','Yes','Yes','Yes','Yes','Yes','No']
 le = preprocessing.LabelEncoder()
-
-
 weather_encoded=le.fit_transform(weather)
 print(weather_encoded)
 temp_encoded=le.fit_transform(temp)
 label=le.fit_transform(play)
 features=list(zip(weather_encoded,temp_encoded))
 model = KNeighborsClassifier(n_neighbors=3)
-
 model.fit(features,label)
-
 #Predict Output
 predicted= model.predict([[0,2]]) # 0:Overcast, 2:Mild
 print(predicted)
